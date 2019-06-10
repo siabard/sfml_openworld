b5091486e3472448e4d96f2789be215c2af0a768
Yeonho Jang <siabard@gmail.com>
2019-06-11 08:40:19 +0900

    메뉴에 글자효과 넣기

--- MovementComponent.cpp
+++ MovementComponent.cpp
@@ -9,23 +9,48 @@ MovementComponent::~MovementComponent() {
 }
 
 void  MovementComponent::update(const float& dt) {
-  // Decelaeration
+  // Decelaeration the sprite and control maximum velocity
+
+  // X Acceleration
   if(this->velocity.x > 0.f) {
+    // Check Max X Velocity
+    if(this->velocity.x > this->maxVelocity)
+      this->velocity.x = this->maxVelocity;
+
+    // X Deaceleration
     this->velocity.x -= this->deceleration;
+
+    // Bound Condition
     if(this->velocity.x < 0.f)
       this->velocity.x = 0.f;
   } else if(this->velocity.x < 0.f) {
+    // Check Max X Velocity
+    if(this->velocity.x < -this->maxVelocity)
+      this->velocity.x = -this->maxVelocity;
+
+    // X Deaceleration
     this->velocity.x += this->deceleration;
+
+    // Bound Condition
     if(this->velocity.x > 0.f)
       this->velocity.x = 0.f;
   }
 
+
   if(this->velocity.y > 0.f) {
+    if(this->velocity.y > this->maxVelocity)
+      this->velocity.y = this->maxVelocity;
+
     this->velocity.y -= this->deceleration;
+
     if(this->velocity.y < 0.f)
       this->velocity.y = 0.f;
   } else if (this->velocity.y < 0.f) {
+    if(this->velocity.y < -this->maxVelocity)
+      this->velocity.y = -this->maxVelocity;
+
     this->velocity.y += this->deceleration;
+
     if(this->velocity.y > 0.f)
       this->velocity.y = 0.f;
   }
@@ -37,22 +62,8 @@ void MovementComponent::move(const float dir_x, const float dir_y, const float& 
   // Acceleration a sprite until it reaches the max velocity
 
   this->velocity.x += this->acceleration * dir_x;
-  if(this->velocity.x > 0.f) {
-    if(this->velocity.x > this->maxVelocity)
-      this->velocity.x = this->maxVelocity;
-  } else if(this->velocity.x < 0.f) {
-    if(this->velocity.x < -this->maxVelocity)
-      this->velocity.x = -this->maxVelocity;
-  }
-
   this->velocity.y += this->acceleration * dir_y;
-  if(this->velocity.y > 0.f) {
-    if(this->velocity.y > this->maxVelocity)
-      this->velocity.y = this->maxVelocity;
-  } else if (this->velocity.y < 0.f) {
-    if(this->velocity.y < -this->maxVelocity)
-      this->velocity.y = -this->maxVelocity;
-  }
+
 }
 
 const sf::Vector2f& MovementComponent::getVelocity() const {
--- States/MainMenuState.cpp
+++ States/MainMenuState.cpp
@@ -90,12 +90,25 @@ void MainMenuState::initFonts() {
 }
 
 void MainMenuState::initButtons() {
-  this->buttons["GAME_STATE_BUTTONS"] = new Button(300, 480, 250, 50, &this->font, "New Game", sf::Color(70, 70, 70, 200),  sf::Color(150, 150, 150, 255),  sf::Color(20, 20, 20, 200));
+  this->buttons["GAME_STATE_BUTTONS"] = new Button(300, 480, 250, 50, &this->font, "New Game", 50,
+                                                   sf::Color(70, 70, 70, 200),  sf::Color(250, 250, 250, 250),  sf::Color(20, 20, 20, 50),
+                                                   sf::Color(70, 70, 70, 0),  sf::Color(150, 150, 150, 0),  sf::Color(20, 20, 20, 0)
+                                                   );
 
-  this->buttons["SETTINGS"] = new Button(300, 580, 250, 50, &this->font, "Settings", sf::Color(70, 70, 70, 200),  sf::Color(150, 150, 150, 255),  sf::Color(20, 20, 20, 200));
+  this->buttons["SETTINGS"] = new Button(300, 580, 250, 50, &this->font, "Settings", 50,
+                                         sf::Color(70, 70, 70, 200),  sf::Color(250, 250, 250, 250),  sf::Color(20, 20, 20, 50),
+                                         sf::Color(70, 70, 70, 0),  sf::Color(150, 150, 150, 0),  sf::Color(20, 20, 20, 0)
+                                         );
 
+  this->buttons["EDITOR_STATE"] = new Button(300, 680, 250, 50, &this->font, "Editor", 50,
+                                             sf::Color(70, 70, 70, 200),  sf::Color(250, 250, 250, 250),  sf::Color(20, 20, 20, 50),
+                                             sf::Color(70, 70, 70, 0),  sf::Color(150, 150, 150, 0),  sf::Color(20, 20, 20, 0)
+                                             );
 
-  this->buttons["EXIT_STATE_BUTTONS"] = new Button(300, 780, 250, 50, &this->font, "Quit", sf::Color(100, 100, 100, 200),  sf::Color(150, 150, 150, 255),  sf::Color(20, 20, 20, 200));
+  this->buttons["EXIT_STATE_BUTTONS"] = new Button(300, 880, 250, 50, &this->font, "Quit", 50,
+                                                   sf::Color(70, 70, 70, 200),  sf::Color(250, 250, 250, 250),  sf::Color(20, 20, 20, 50),
+                                                   sf::Color(70, 70, 70, 0),  sf::Color(150, 150, 150, 0),  sf::Color(20, 20, 20, 0)
+                                                   );
 
 }
 
